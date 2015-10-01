0f37de6f1f0b1ada1e982a3d5b149828e8ceba3e
pknovo <p.kamdem@fromnovo.com>
2015-10-01 18:50:17 -0400

    initial commit

--- core/components/api/core-api-connector/core-api-connector.js
+++ core/components/api/core-api-connector/core-api-connector.js
@@ -1,105 +1,30 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 (function (scope, Polymer) {
+    var CONNECTION_RETRY_MAX = 10;
+    var CONNECTION_RETRY_DELAY = 1000;
     var URL_PREFIX = "ws://";
-    var CoreApiConnector = (function () {
-        function CoreApiConnector() {
+    var CoreApiConnectorPolymer = (function () {
+        function CoreApiConnectorPolymer() {
         }
-        Object.defineProperty(CoreApiConnector.prototype, "is", {
+        Object.defineProperty(CoreApiConnectorPolymer.prototype, "is", {
             get: function () {
                 return "core-api-connector";
             },
             enumerable: true,
             configurable: true
         });
-        Object.defineProperty(CoreApiConnector.prototype, "properties", {
-            get: function () {
-                return {
-                    auto: {
-                        type: Boolean,
-                        value: false
-                    },
-                    url: {
-                        type: String,
-                        value: "echo.websocket.org"
-                    }
-                };
-            },
-            enumerable: true,
-            configurable: true
-        });
-        Object.defineProperty(CoreApiConnector.prototype, "connected", {
-            get: function () {
-                return this._connector !== null && this._connector.readyState !== WebSocket.OPEN;
-            },
-            enumerable: true,
-            configurable: true
-        });
-        CoreApiConnector.prototype.created = function () {
-            this._connector = null;
-        };
-        CoreApiConnector.prototype.attached = function () {
-            if (this.auto) {
-                this.open();
-            }
-        };
-        CoreApiConnector.prototype.detached = function () {
-            console.log("detached...");
-        };
-        CoreApiConnector.prototype.connect = function () {
-            var _this = this;
-            return new Promise(function (resolve, reject) {
-                if (_this.connected) {
-                    resolve("oups");
-                }
-                else {
-                    _this.open().then(function () { resolve("connected"); });
-                }
-            });
-        };
-        CoreApiConnector.prototype._createConnector = function () {
-            return new WebSocket("" + URL_PREFIX + this.url);
-        };
-        CoreApiConnector.prototype.open = function () {
-            var _this = this;
-            var ws = this._createConnector();
-            var socketOpen$ = Rx.Observer.create(function (timestamp) {
-                console.log("Connection open at " + timestamp);
-                ws.send("Hello");
-            });
-            var socketResponse$ = Rx.Observer.create(function (payload) {
-                console.log("Sending payload... " + payload);
-                ws.send(payload);
-            });
-            var socket$ = Rx.Observable.create(function (observer) {
-                ws.onopen = function () { socketOpen$.onNext(Date.now()); };
-                ws.onmessage = observer.onNext.bind(observer);
-                ws.onerror = observer.onError.bind(observer);
-                ws.onclose = observer.onCompleted.bind(observer);
-            }).take(2);
-            socket$.subscribe(function (event) {
-                _this.$.span.textContent = event.data;
-                setTimeout(function () { socketResponse$.onNext("Message - " + Date.now()); }, 5000);
-            }, function (event) {
-                console.log("Error occured... " + event.reason);
-            }, function () {
-                console.log("Completed....");
-            });
-            return new Promise(function (resolve, reject) {
-                _this._connector = new WebSocket("" + URL_PREFIX + _this.url);
-                _this._connector.onopen = function () {
-                    resolve("opened");
-                };
-            });
-        };
-        CoreApiConnector.prototype.getConnector = function () {
-            var _this = this;
-            return Rx.Observable.create(function (observer) {
-                _this._connector = new WebSocket("" + URL_PREFIX + _this.url);
-                _this._connector.onopen = function () {
-                    observer.onNext(_this._connector.readyState);
-                };
-            });
-        };
-        return CoreApiConnector;
+        return CoreApiConnectorPolymer;
     })();
+    var CoreApiConnector = (function (_super) {
+        __extends(CoreApiConnector, _super);
+        function CoreApiConnector() {
+            _super.apply(this, arguments);
+        }
+        return CoreApiConnector;
+    })(CoreApiConnectorPolymer);
     Polymer(CoreApiConnector);
 })(window, Polymer);
--- core/components/api/core-api-connector/core-api-connector.ts
+++ core/components/api/core-api-connector/core-api-connector.ts
@@ -1,34 +1,16 @@
 (( scope: Window, Polymer: polymer.PolymerStatic ) => {
 
+  const CONNECTION_RETRY_MAX: number = 10;
+  const CONNECTION_RETRY_DELAY: number = 1000;
   const URL_PREFIX: string = "ws://";
 
-  /**
-   * Core Api Connector custom element
-   * @class
-   */
-  class CoreApiConnector implements polymer.Base {
-    $: any;
-    public connection$: Rx.Observable<boolean>;
-    public open$: Rx.Observable<boolean>;
-    /**
-     * Indicates if this connection should be initiated automaticly.
-     *
-     * @attribute auto
-     * @member
-     * @type {boolean}
-     * @default true
-     * @public
-     */
-    public auto: boolean;
-    /**
-     * Indicates if this connection should be initiated automaticly.
-     *
-     * @attribute auto
-     * @member
-     * @type {boolean}
-     * @default true
-     * @public
-     */
+  class CoreApiConnectorPolymer implements polymer.Base  {
+    get is(): string {
+      return "core-api-connector";
+    }
+  }
+
+  class CoreApiConnector extends CoreApiConnectorPolymer {
     public url: string;
     /**
      * Indicates if this connection should be initiated automaticly.
@@ -37,136 +19,9 @@
      * @member
      * @type {boolean}
      * @default true
-     * @private
      */
-    private _connector: WebSocket;
-    /**
-     * [is description]
-     * @return {string} [description]
-     */
-    get is(): string {
-      return "core-api-connector";
-    }
-    /**
-     * Configures Custom Element's properties
-     *
-     * @return {Object} Metadata regarding your Custom Element's properties,
-     * which can then be accessed via an API for use by other Polymer features
-     */
-    get properties(): { [ prop: string ]: polymer.PropObjectType } {
-      return {
-        auto: {
-          type: Boolean,
-          value: false
-        },
-        url: {
-          type: String,
-          value: "echo.websocket.org"
-        }
-      };
-    }
-    /**
-     * [connected description]
-     * @return {boolean} [description]
-     */
-    get connected(): boolean {
-      return this._connector !== null && this._connector.readyState !== WebSocket.OPEN;
-    }
-    /**
-     * Invoked after an element instance is created and its definition
-     * is registered.
-     *
-     * @member
-     * @private
-     * @function created
-     */
-    created(): void {
-      this._connector = null;
-    }
-    /**
-     * Invoked after an element instance is inserted into a document and this
-     * document has a browsing context.
-     *
-     * @member
-     * @private
-     * @function attached
-     */
-    attached(): void {
-      if ( this.auto ) {
-        this.open();
-      }
-    }
-    /**
-     * Invoked after an element instance is removed from the document and
-     * this document has a browsing context.
-     *
-     * @member
-     * @private
-     * @function detached
-     */
-    detached(): void {
-      console.log("detached...");
-    }
-
-    connect(): Promise<string> {
-      return new Promise<string>(( resolve, reject ) => {
-        if ( this.connected ) {
-          resolve( "oups" );
-        } else {
-          this.open().then(() => { resolve( "connected" ); });
-        }
-      });
-    }
-
-    _createConnector(): WebSocket {
-      return new WebSocket( `${URL_PREFIX}${this.url}` );
-    }
-
-    open(): Promise<string>  {
-      let ws: WebSocket = this._createConnector();
-
-      let socketOpen$ = Rx.Observer.create<any>((timestamp: number) => {
-        console.log(`Connection open at ${timestamp}`);
-        ws.send("Hello");
-      });
-
-      let socketResponse$ = Rx.Observer.create<string>((payload: string) => {
-        console.log(`Sending payload... ${payload}`);
-        ws.send(payload);
-      });
-
-      let socket$: Rx.Observable<any> = Rx.Observable.create<any>((observer) => {
-        ws.onopen = () => {  socketOpen$.onNext( Date.now() ); };
-        ws.onmessage = observer.onNext.bind(observer);
-        ws.onerror = observer.onError.bind(observer);
-        ws.onclose = observer.onCompleted.bind(observer);
-      }).take(2);
-
-      socket$.subscribe((event: MessageEvent) => {
-        this.$.span.textContent = event.data;
-        setTimeout( () => { socketResponse$.onNext(`Message - ${ Date.now() }`); }, 5000 );
-      }, (event: CloseEvent) => {
-          console.log(`Error occured... ${event.reason}`);
-      }, () => {
-          console.log(`Completed....`);
-      });
-
-      return new Promise<string>(( resolve, reject ) => {
-        this._connector = new WebSocket( `${URL_PREFIX}${this.url}` );
-        this._connector.onopen = () => {
-          resolve( "opened" );
-        };
-      });
-    }
-
-    getConnector(): Rx.Observable<number> {
-      return Rx.Observable.create<number>( observer => {
-        this._connector = new WebSocket( `${URL_PREFIX}${this.url}` );
-        this._connector.onopen = () => {
-           observer.onNext(this._connector.readyState);
-        };
-      });
-    }
+    public auto: boolean;
+    private _connected: boolean;
   }
 
   Polymer( CoreApiConnector );
